491295b5ac9760e22400ab76d0ff09b0e07b3c45
Aleksandr Kulagin <InCo_O@mail.ru>
2020-01-30 13:53:02 +0300

    register instead boot method

--- src/AdvancedTableRowServiceProvider.php
+++ src/AdvancedTableRowServiceProvider.php
@@ -13,7 +13,7 @@ class AdvancedTableRowServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function register()
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-table-row', __DIR__ . '/../dist/js/tool.js');
